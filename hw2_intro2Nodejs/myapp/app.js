370b5ee77a9175c5c4c8215a6f2fca9903f7e43b
Nian <nianzuomin100@163.com>
2015-06-19 17:10:19 +0800

    merge

--- hw2_intro2Nodejs/myapp/app.js
+++ hw2_intro2Nodejs/myapp/app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/test');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
